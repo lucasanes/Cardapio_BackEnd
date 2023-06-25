5ffd66281fd360d5b2dc20ffc1ab207847180e2a
SkyLuke11 <sky.lucas003@gmail.com>
2023-06-25 15:52:31 -0300

    ac

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const AppError = require("./utils/AppError");
 const cors = require("cors");
 const prisma = require("./modules/database/prisma");
 require("dotenv").config();
+const simpleGit = require("simple-git")
 
 const app = express();
 
@@ -26,6 +27,25 @@ app.use((error, request, response, next) => {
 
 app.use(cors());
 
-app.listen(process.env.PORT || 3000, "0.0.0.0", () =>
+app.post('/webhook', (req, res) => {
+  // Executar o comando git pull no diretório do repositório
+  simpleGit("D:/Users/Lucas/Desktop/Programador/ProjetoCardapio/Cardapio_BackEnd")  // Substitua pelo caminho correto do repositório no sistema do colaborador
+    .pull((err, update) => {
+      if (err) {
+        console.error('Erro ao executar git pull:', err);
+        res.sendStatus(500);
+      } else {
+        if (update && update.summary.changes) {
+          console.log('Repositório atualizado');
+          res.sendStatus(200);
+        } else {
+          console.log('Nenhuma alteração no repositório');
+          res.sendStatus(204);
+        }
+      }
+    });
+});
+
+app.listen(process.env.PORT || 8080, "0.0.0.0", () =>
   console.log("Server is running")
 );
