318c6dc10b155c48738d5fef77d8a8573a5b0a18
SkyLuke11 <sky.lucas003@gmail.com>
2023-07-11 18:55:35 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://cardapios-admin.vercel.app/'
+  'https://cardapioadmin.vercel.app/'
 ];
 
 const corsOptions = {
